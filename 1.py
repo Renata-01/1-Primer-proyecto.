c08428ba086d82876a86f19c43f96b2121698e8c
Renata-01 <renarosas01@gmail.com>
2025-05-30 17:52:57 -0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+# 1.- ---------------- Encabezado ----------------------------------------------
+'''
+Programa:
+Autor:
+Fecha:
+Descripción:
+Modificado por:
+Fecha de modificación:
+Hora:
+'''
+# 2.- ---------------- Importación de módulos y bibliotecas --------------------
+# 3.- ---------------- Definición de funciones o clases ------------------------    
+# 4.- ---------------- Variables u objetos globales ----------------------------
+# 5.- ---------------- Bloque principal ----------------------------------------
+# 6.- ---------------- Documentación y comentarios -----------------------------
