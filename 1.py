56546b6b41f8c519f019a71196cc21d84f929691
Renata-01 <renarosas01@gmail.com>
2025-05-30 19:00:30 -0600

    Se agregaron comandos

--- 1.py
+++ 1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # 1.- ---------------- Encabezado ----------------------------------------------
 '''
 Programa:
@@ -10,25 +9,13 @@ Fecha de modificación:
 Hora:
 '''
 # 2.- ---------------- Importación de módulos y bibliotecas --------------------
+import tkinter as tk
+from tkinter import messagebox
 # 3.- ---------------- Definición de funciones o clases ------------------------    
 # 4.- ---------------- Variables u objetos globales ----------------------------
 # 5.- ---------------- Bloque principal ----------------------------------------
+if _name_ == "_main_":
+    aplicacion = MenuPrincipal()
+    aplicacion.ventana.mainloop()
 # 6.- ---------------- Documentación y comentarios -----------------------------
-=======
-# 1.- ---------------- Encabezado ----------------------------------------------
-'''
-Programa:
-Autor:
-Fecha:
-Descripción:
-Modificado por:
-Fecha de modificación:
-Hora:
-'''
-# 2.- ---------------- Importación de módulos y bibliotecas --------------------
-# 3.- ---------------- Definición de funciones o clases ------------------------    
-# 4.- ---------------- Variables u objetos globales ----------------------------
-# 5.- ---------------- Bloque principal ----------------------------------------
-# 6.- ---------------- Documentación y comentarios -----------------------------
-#
->>>>>>> 631883d (prueba 1)
+#hola
