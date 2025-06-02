b734a7936c068251d8e0895f1c68b4bffddf7437
Renata-01 <renarosas01@gmail.com>
2025-06-02 12:56:35 -0600

    Prueba 1

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # 1.- ---------------- Encabezado ----------------------------------------------
 '''
 Programa:
@@ -13,3 +14,21 @@ Hora:
 # 4.- ---------------- Variables u objetos globales ----------------------------
 # 5.- ---------------- Bloque principal ----------------------------------------
 # 6.- ---------------- Documentación y comentarios -----------------------------
+=======
+# 1.- ---------------- Encabezado ----------------------------------------------
+'''
+Programa:
+Autor:
+Fecha:
+Descripción:
+Modificado por:
+Fecha de modificación:
+Hora:
+'''
+# 2.- ---------------- Importación de módulos y bibliotecas --------------------
+# 3.- ---------------- Definición de funciones o clases ------------------------    
+# 4.- ---------------- Variables u objetos globales ----------------------------
+# 5.- ---------------- Bloque principal ----------------------------------------
+# 6.- ---------------- Documentación y comentarios -----------------------------
+#
+>>>>>>> 631883d (prueba 1)
